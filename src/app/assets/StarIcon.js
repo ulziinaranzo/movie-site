beedf08ded0ddcd98c55a48c706b4c8759e3a2a9
24LP2564 24LP2564 <24LP2564@G401-CC0101.local>
2025-03-25 18:09:21 +0800

    lol

--- src/app/assets/StarIcon.js
+++ src/app/assets/StarIcon.js
@@ -1,8 +1,19 @@
 export const StarIcon = () => {
-    return (
-        <svg width="16" height="18" viewBox="0 0 16 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M8.00065 3.33337L10.0607 7.50671L14.6673 8.18004L11.334 11.4267L12.1207 16.0134L8.00065 13.8467L3.88065 16.0134L4.66732 11.4267L1.33398 8.18004L5.94065 7.50671L8.00065 3.33337Z" fill="#FDE047" stroke="#FDE047" strokeLinecap="round" strokeLinejoin="round"/>
-</svg>
-    )
-
-}
+  return (
+    <svg
+      width="16"
+      height="18"
+      viewBox="0 0 16 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M8.00065 3.33337L10.0607 7.50671L14.6673 8.18004L11.334 11.4267L12.1207 16.0134L8.00065 13.8467L3.88065 16.0134L4.66732 11.4267L1.33398 8.18004L5.94065 7.50671L8.00065 3.33337Z"
+        fill="#FDE047"
+        stroke="#FDE047"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
