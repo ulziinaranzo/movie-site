4e627ca4150ea0874d53be3ef723ddc937daf488
ulziinaranzo <ulziinaranzorig@gmail.com>
2025-06-15 13:18:06 +0800

    check

--- src/app/_components/GenreProvider.tsx
+++ src/app/_components/GenreProvider.tsx
@@ -1,47 +1,43 @@
-"use client"
+"use client";
 import { useEffect, useState, PropsWithChildren, createContext } from "react";
 import { useContext } from "react";
 import axios from "axios";
-
-const Access_Token =
-  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YWI2NTUxNzRmNThkNWM0MzgzZDJiMzQzZTM1NzMxNCIsIm5iZiI6MTc0MzE1MDY0NC4xMzUsInN1YiI6IjY3ZTY1ZTM0M2U2NWM4ZWE4OGJhM2EwYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Ys86E8XJOdTpg5ll351TU3CKG9veVwrbjMneJdAxIHg";
+import { Access_Token } from "../components/Types";
 
 type Genre = {
-    id: number;
-    name: string;
-}
+  id: number;
+  name: string;
+};
 
 type GenreContextType = {
-    genres: Genre[];
-}
+  genres: Genre[];
+};
 
 const GenreContext = createContext<GenreContextType>({
-    genres: []
+  genres: [],
 });
 
 export const GenreProvider = ({ children }: PropsWithChildren<{}>) => {
-    const [genres, setGenres] = useState<Genre[]>([]);
+  const [genres, setGenres] = useState<Genre[]>([]);
 
-    useEffect(() => {
-        const getGenres = async () => {
-            const { data } = await axios.get(
-                `https://api.themoviedb.org/3/genre/movie/list?language=en`, 
-                {
-                    headers: {
-                        Authorization: `Bearer ${Access_Token}`
-                    }
-                }
-            );
-            setGenres(data.genres);
-        };
-        getGenres();
-    }, []);
+  useEffect(() => {
+    const getGenres = async () => {
+      const { data } = await axios.get(
+        `https://api.themoviedb.org/3/genre/movie/list?language=en`,
+        {
+          headers: {
+            Authorization: `Bearer ${Access_Token}`,
+          },
+        }
+      );
+      setGenres(data.genres);
+    };
+    getGenres();
+  }, []);
 
-    return (
-        <GenreContext.Provider value={{ genres }}>
-            {children}
-        </GenreContext.Provider>
-    );
-}
+  return (
+    <GenreContext.Provider value={{ genres }}>{children}</GenreContext.Provider>
+  );
+};
 
 export const useGenres = () => useContext(GenreContext);
